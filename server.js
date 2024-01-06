16b00c13176998cb0ff947ac4287746c96a5ef39
Austin Oler <oleraustin@gmail.com>
2024-01-05 19:15:56 -0500

    added API routes

--- server.js
+++ server.js
@@ -20,3 +20,24 @@ app.get('/notes', (req, res) => {
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
+
+  // API Routes
+app.get('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+    res.json(notes);
+  });
+  
+  app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+  
+    // Generate a unique ID for the new note (you may use npm package like uuid)
+    newNote.id = generateUniqueId(); 
+  
+    notes.push(newNote);
+  
+    fs.writeFileSync('db.json', JSON.stringify(notes));
+  
+    res.json(newNote);
+  });
+  
